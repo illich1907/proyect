d282f068c9e109fcee624a934b3e5fe623ed8e5c
illich1907 <anastasiazatovkanuk@gmail.com>
2024-07-03 16:46:55 +0300

    Edit Main

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 int function(const char* str, const char* word) {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    while (str[b] != '\0') {
+        b++;
+    }
+
+    while (word[c] != '\0') {
+        c++;
+    }
+
+    for (int i = 0; i <= b - c; i++) {
+        int j = 0;
+        while (j < c && str[i + j] == word[j]) {
+            j++;
+        }
+        if (j == c) {
+            a++;
+        }
+    }
+
+    return a;
 }
 int main() 
 {
